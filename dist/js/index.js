b89bfbfb75f32e7119b13f61d2f9c4e2c5321004
Hong Baojin <13809735212@163.com>
2019-05-07 18:23:43 +0800

    write to index_3

---
+++ dist/js/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+$(function () {
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    var scrollHeight = $(document).height();
+    var windowHeight = $(this).height();
+    if (scrollTop + windowHeight == scrollHeight) {
+      // alert("已经到最底部了！");
+      // 通过stop() 方法为被选元素停止当前正在运行的动画
+      $(".kit .footer").stop(true, true).slideDown();
+    } else {
+      $(".kit .footer").stop(true, true).slideUp();
+    }
+  });
+});
